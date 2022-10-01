f53b477ada74dced8068a0212feeb8a656efbb97
Waihon Yew <yewwaihon@gmail.com>
2022-10-01 18:43:20 +0800

    Fix the bug in calculating area

--- leetcode/container-with-most-water.rb
+++ leetcode/container-with-most-water.rb
@@ -4,7 +4,7 @@ def max_area(height)
   max_area = 0
   for p1 in 0..(height.length - 2)
     for p2 in (p1 + 1)..(height.length - 1)
-      area = [height[p1], height[p2]].min * (p2 - 1)
+      area = [height[p1], height[p2]].min * (p2 - p1)
       max_area = [max_area, area].max
     end
   end
