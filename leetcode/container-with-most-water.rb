a322e063bf92e1d9332a9fde3f68bf84ff9abf01
Waihon Yew <yewwaihon@gmail.com>
2022-10-01 21:52:28 +0800

    Use variables for better readability

--- leetcode/container-with-most-water.rb
+++ leetcode/container-with-most-water.rb
@@ -1,13 +1,15 @@
 # @param {Integer[]} height
 # @return {Integer}
-def max_area(height)
+def max_area(heights)
   max_area = 0
   p1 = 0
-  p2 = height.length - 1
+  p2 = heights.length - 1
   while p1 < p2
-    area = [height[p1], height[p2]].min * (p2 - p1)
+    height = [heights[p1], heights[p2]].min
+    width = p2 - p1
+    area = height * width
     max_area = [max_area, area].max
-    if height[p1] <= height[p2]
+    if heights[p1] <= heights[p2]
       p1 += 1
     else
       p2 -= 1
