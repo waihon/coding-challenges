afeb33c1caf9993b6361a9d512ba559a9638e6d9
Waihon Yew <yewwaihon@gmail.com>
2022-10-02 22:48:14 +0800

    Reverse the if condition for better readability

--- leetcode/trapping-rain-water-optimal.rb
+++ leetcode/trapping-rain-water-optimal.rb
@@ -11,20 +11,20 @@ def trap(heights)
   while p_left < p_right
     if heights[p_left] <= heights[p_right]
       current_height = heights[p_left]
-      if current_height < max_left
+      if current_height >= max_left
+        max_left = current_height
+      else current_height < max_left
         current_water = max_left - current_height
         total_water += current_water
-      else
-        max_left = current_height
       end
       p_left += 1
     else
       current_height = heights[p_right]
-      if current_height < max_right
+      if current_height >= max_right
+        max_right = current_height
+      else 
         current_water = max_right - current_height
         total_water += current_water
-      else
-        max_right = current_height
       end
       p_right -= 1
     end
