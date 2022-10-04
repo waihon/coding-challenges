a493d50ff4d61349a79309240e57ba8746e21141
Waihon Yew <yewwaihon@gmail.com>
2022-10-04 13:54:48 +0800

    Brute force ver of longest unique substring
    (without repeating characters)

---
+++ leetcode/longest-substring-without-repeating-characters-brute-force.rb
@@ -0,0 +1,21 @@
+# @param {String} s
+# @return {Integer}
+def length_of_longest_substring(s)
+  max_length = 0
+  
+  for p1 in 0..(s.length - 1)
+    unique_chars = Hash.new(false)
+    p2 = p1
+    while p2 <= (s.length - 1)
+      if unique_chars[s[p2]] 
+        break
+      else
+        unique_chars[s[p2]] = true
+        max_length = [max_length, unique_chars.length].max
+        p2 += 1
+      end
+    end
+  end
+  
+  return max_length
+end
