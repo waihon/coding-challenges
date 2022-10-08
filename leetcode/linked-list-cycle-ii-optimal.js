256c4ae0bda352832859a136044ba7221c788409
Waihon Yew <yewwaihon@gmail.com>
2022-10-08 13:05:39 +0800

    Detect cycle using Floyd's algorithm written in JS

---
+++ leetcode/linked-list-cycle-ii-optimal.js
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/linked-list-cycle-ii
+// Given the head of a linked list, return the node where the cycle begins.
+// If there is no cycle, return null.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const detectCycle = function(head) {
+  if (head === null) {
+    return null;
+  }
+  
+  let tortoise = head, hare = head;
+  
+  while (hare && hare.next) {
+    tortoise = tortoise.next;  // toitoise move 1 step
+    hare = hare.next.next; // hair more 2 steps
+ 
+    if (tortoise === hare) { // found a meeting point, i.e. cycle detected
+      let p1 = head;
+      let p2 = tortoise;
+      while (p1 !== p2) {
+        p1 = p1.next;
+        p2 = p2.next;
+      }
+      return p1;
+    }
+  }
+
+  return null;
+};
