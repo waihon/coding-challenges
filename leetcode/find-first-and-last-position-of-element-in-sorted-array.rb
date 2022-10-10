359bb2c7a724135904feb40ccb0fc918d3c5fb69
Waihon Yew <yewwaihon@gmail.com>
2022-10-10 12:19:55 +0800

    Find first and last pos of element in sorted array

---
+++ leetcode/find-first-and-last-position-of-element-in-sorted-array.rb
@@ -0,0 +1,68 @@
+# https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+# Given an array of integers nums sorted in non-decreasing order,
+# find the starting and ending position of a given target value.
+# If target is not found in the array, return [-1, -1].
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def search_range(nums, target)
+  left = 0; right = nums.length - 1
+  
+  while left <= right
+    mid = ((left + right) / 2).floor
+    found_value = nums[mid]
+    if found_value == target
+      return build_array(nums, target, mid)
+    elsif found_value > target
+      right = mid - 1
+    else # found_value < target
+      left = mid + 1
+    end
+  end
+  
+  # not found
+  return [-1, -1]
+end
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @param {Integer} mid
+# return {Integer[]}
+def build_array(nums, target, mid)
+  # Search for any elements to the left and with the
+  # same target value.
+  p1 = mid - 1
+  left = nil
+  while p1 >= 0
+    if nums[p1] == target
+      left = p1
+      p1 -= 1
+    else
+      break
+    end
+  end
+  
+  # Search for any elements to the right and with the
+  # same target value.
+  p2 = mid + 1
+  right = nil
+  while p2 <= nums.length - 1
+    if nums[p2] == target
+      right = p2
+      p2 += 1
+    else
+      break
+    end
+  end
+  
+  if left && right
+    return [left, right]
+  elsif left && !right
+    return [left, mid]
+  elsif right & !left
+    return [mid, right]
+  else
+    return [mid]
+  end
+end
