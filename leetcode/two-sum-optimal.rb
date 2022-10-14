52932eb8a475eadadca39ca77a208efc73f79a5b
Waihon Yew <yewwaihon@gmail.com>
2022-10-15 05:37:54 +0800

    Add problem statement

--- leetcode/two-sum-optimal.rb
+++ leetcode/two-sum-optimal.rb
@@ -1,4 +1,7 @@
 # https://leetcode.com/problems/two-sum
+# Given an array of integers nums and an integer target,
+# return indices of the two numbers such that they add up
+# to target.
 
 # @param {Integer[]} nums
 # @param {Integer} target
