9ba9d63bcb62779d4998d4da9633f999e37f7c0a
Waihon Yew <yewwaihon@gmail.com>
2022-10-15 16:11:02 +0000

    Print positive message when all test cases passed

--- leetcode/arrays/two-sum-optimal.rb
+++ leetcode/arrays/two-sum-optimal.rb
@@ -35,12 +35,18 @@ def two_sum(nums, target)
 end
 
 if __FILE__ == $0
-  raise RuntimeError unless two_sum([1, 3, 7, 9, 2], 11) == [3, 4]
-  raise RuntimeError unless two_sum([1, 3, 7, 9, 2], 25) == nil
-  raise RuntimeError unless two_sum([], 1) == nil
-  raise RuntimeError unless two_sum([5], 5) == nil
-  raise RuntimeError unless two_sum([1, 6], 7) == [0, 1]
-  raise RuntimeError unless two_sum([2, 7, 11, 15], 9) == [0, 1]
-  raise RuntimeError unless two_sum([3, 2, 4], 6) == [1, 2]
-  raise RuntimeError unless two_sum([3, 3], 6) == [0, 1]
+  begin
+    raise RuntimeError unless two_sum([1, 3, 7, 9, 2], 11) == [3, 4]
+    raise RuntimeError unless two_sum([1, 3, 7, 9, 2], 25) == nil
+    raise RuntimeError unless two_sum([], 1) == nil
+    raise RuntimeError unless two_sum([5], 5) == nil
+    raise RuntimeError unless two_sum([1, 6], 7) == [0, 1]
+    raise RuntimeError unless two_sum([2, 7, 11, 15], 9) == [0, 1]
+    raise RuntimeError unless two_sum([3, 2, 4], 6) == [1, 2]
+    raise RuntimeError unless two_sum([3, 3], 6) == [0, 1]
+  rescue RuntimeError => e
+    puts e.message + ": " + e.backtrace.inspect
+  else
+    puts "All test cases passed!"
+  end    
 end
