d72cbca2cb58fd3ea62f079aac46a7680743d59d
Waihon Yew <yewwaihon@gmail.com>
2022-10-15 16:18:13 +0000

    Add test cases to trapping-rain-water

--- leetcode/arrays/trapping-rain-water-optimal.rb
+++ leetcode/arrays/trapping-rain-water-optimal.rb
@@ -37,3 +37,18 @@ def trap(heights)
   
   return total_water
 end
+
+if __FILE__ == $0
+  begin
+    raise RuntimeError unless trap([0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]) == 8
+    raise RuntimeError unless trap([]) == 0
+    raise RuntimeError unless trap([3]) == 0
+    raise RuntimeError unless trap([3, 4, 3]) == 0
+    raise RuntimeError unless trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]) == 6
+    raise RuntimeError unless trap([4, 2, 0, 3, 2, 5]) == 9
+  rescue RuntimeError => e
+    puts e.message + ": " + e.backtrace.inspect
+  else
+    puts "All test cases passed!"
+  end    
+end
