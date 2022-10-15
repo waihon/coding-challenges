2ffcf614eb0c4e2f00a56d79ef772852ad0cc419
Waihon Yew <yewwaihon@gmail.com>
2022-10-15 12:15:44 +0800

    Add problem statement

--- leetcode/trapping-rain-water-optimal.rb
+++ leetcode/trapping-rain-water-optimal.rb
@@ -1,3 +1,8 @@
+# https://leetcode.com/problems/trapping-rain-water/
+# Given n non-negative integers representing an elevation map
+# where the width of each bar is 1, compute how much water
+# it can trap after raining.
+
 # @param {Integer[]} heights
 # @return {Integer}
 def trap(heights)
