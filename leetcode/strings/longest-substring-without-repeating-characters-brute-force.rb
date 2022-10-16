d77b29160e114378215edc86d43dd5bb85746ac2
Waihon Yew <yewwaihon@gmail.com>
2022-10-16 01:48:16 +0000

    Add test cases to longest-substring

--- leetcode/strings/longest-substring-without-repeating-characters-brute-force.rb
+++ leetcode/strings/longest-substring-without-repeating-characters-brute-force.rb
@@ -25,3 +25,19 @@ def length_of_longest_substring(s)
   
   return longest 
 end
+
+if __FILE__ == $0
+  begin
+    raise RuntimeError unless length_of_longest_substring("abccabb") == 3
+    raise RuntimeError unless length_of_longest_substring("cccccc") == 1
+    raise RuntimeError unless length_of_longest_substring("") == 0
+    raise RuntimeError unless length_of_longest_substring("abcbda") == 4
+    raise RuntimeError unless length_of_longest_substring("abcabcbb") == 3
+    raise RuntimeError unless length_of_longest_substring("bbbbb") == 1
+    raise RuntimeError unless length_of_longest_substring("pwwkew") == 3
+  rescue RuntimeError => e
+    puts e.message + ": " + e.backtrace.inspect
+  else
+    puts "All test cases passed!"
+  end    
+end
