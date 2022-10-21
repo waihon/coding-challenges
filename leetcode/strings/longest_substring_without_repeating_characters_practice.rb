3ebaa5066d1380a76f65c017ffcc029041ac86ed
Waihon Yew <yewwaihon@gmail.com>
2022-10-21 08:49:12 +0000

    Practice longest-substring

--- leetcode/strings/longest_substring_without_repeating_characters_practice.rb
+++ leetcode/strings/longest_substring_without_repeating_characters_practice.rb
@@ -6,17 +6,48 @@
 # @return {Integer}
 def length_of_longest_substring(s)
   # Optimization
+  return s.length if s.length <= 1
+
+  left = 0
+  right = 0
+  seen_char_index_map = Hash.new
+  longest = 0
+
+  for right in 0..(s.length - 1)
+    current_char = s[right]
+    prev_seen_char_index = seen_char_index_map[current_char]
+    # We shouldn't advance `left` if the found index is before `left`,
+    # which indicates the corresponding character is outside of
+    # current window.
+    if prev_seen_char_index && prev_seen_char_index >= left
+      left = prev_seen_char_index + 1 
+    end
+
+    seen_char_index_map[current_char] = right
+
+    current_length = right - left + 1
+    longest = [longest, current_length].max
+  end
+
+  return longest
 end
 
 if __FILE__ == $0
   begin
-    raise RuntimeError unless length_of_longest_substring("abccabb") == 3
-    raise RuntimeError unless length_of_longest_substring("cccccc") == 1
-    raise RuntimeError unless length_of_longest_substring("") == 0
-    raise RuntimeError unless length_of_longest_substring("abcbda") == 4
-    raise RuntimeError unless length_of_longest_substring("abcabcbb") == 3
-    raise RuntimeError unless length_of_longest_substring("bbbbb") == 1
-    raise RuntimeError unless length_of_longest_substring("pwwkew") == 3
+    test_cases = [[1, "abcbdaac", 4],
+                  [2, "abccabb", 3],
+                  [3, "cccccc", 1],
+                  [4, "", 0],
+                  [5, "abcbda", 4],
+                  [6, "abcabcbb", 3],
+                  [7, "bbbbb", 1],
+                  [8, "pwwkew", 3]]
+    test_cases.each do |case_num, input, expected|
+      unless (result = length_of_longest_substring(input)) == expected 
+        puts "Test Case: #{case_num}, Input: #{input}, Actual Eesult: #{result}, Expected: #{expected}"
+        raise RuntimeError
+      end
+    end
   rescue RuntimeError => e
     puts e.message + ": " + e.backtrace.inspect
   else
