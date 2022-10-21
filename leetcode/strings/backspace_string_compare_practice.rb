4134d7a547988493cbf07924d729f8c2aad3afbb
Waihon Yew <yewwaihon@gmail.com>
2022-10-21 01:14:23 +0000

    Practice backspace-string-compare

--- leetcode/strings/backspace_string_compare_practice.rb
+++ leetcode/strings/backspace_string_compare_practice.rb
@@ -9,10 +9,37 @@
 def backspace_compare(s, t)
   # s: "abc#d" => "abd"
   # t: "abzz##d" => "abc"
+  ps = s.length - 1
+  pt = t.length - 1
+
+  while ps >= 0 || pt >= 0
+    if s[ps] == "#" || t[pt] == "#"
+      if s[ps] == "#"
+        backcount = 2
+        while backcount > 0
+          backcount -= 1
+          ps -= 1
+          backcount += 2 if s[ps] == "#"
+        end
+      end
+      if t[pt] == "#" 
+        backcount = 2
+        while backcount > 0
+          backcount -= 1
+          pt -= 1
+          backcount += 2 if s[pt] == "#"
+        end
+      end
+    else
       # Both string are at a postion in which their character
       # is sure to be in the final string.
-      
-      # Ruby's -1 points to the last character
+      if s[ps] != t[pt]
+        return false
+      else
+        ps -= 1
+        pt -= 1
+      end
+    end
   end
   
   return true
