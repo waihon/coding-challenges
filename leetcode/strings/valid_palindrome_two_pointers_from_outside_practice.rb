64d3e62698e977bb0063762f2e30265e32a0d206
Waihon Yew <yewwaihon@gmail.com>
2022-10-22 03:59:37 +0000

    Practice valid-palindrome-outside

--- leetcode/strings/valid_palindrome_two_pointers_from_outside_practice.rb
+++ leetcode/strings/valid_palindrome_two_pointers_from_outside_practice.rb
@@ -8,22 +8,43 @@
 # @param {String} s
 # @return {Boolean}
 def is_palindrome(s)
+  s = s.gsub(/[^A-Za-z0-9]/, "").downcase 
+
+  left = 0
+  right = s.length - 1
+
+  while left <= right
+    if s[left] != s[right]
+      return false
+    end
+    left += 1
+    right -= 1
+  end
+
+  return true
 end
 
 if __FILE__ == $0
   begin
-    raise RuntimeError unless is_palindrome("aabaa") == true
-    raise RuntimeError unless is_palindrome("aabbaa") == true
-    raise RuntimeError unless is_palindrome("abc") == false
-    raise RuntimeError unless is_palindrome("a") == true 
-    raise RuntimeError unless is_palindrome("") == true
-    raise RuntimeError unless is_palindrome(" ") == true
-    raise RuntimeError unless is_palindrome("A man, a plan, a canal: Panama") == true
-    raise RuntimeError unless is_palindrome("race a car") == false
-    raise RuntimeError unless is_palindrome("race car") == true 
+    test_cases = [
+      [1, "aabaa", true],
+      [2, "aabbaa", true],
+      [3, "abc", false],
+      [4, "a", true],
+      [5, "", true],
+      [6, " ", true],
+      [7, "A man, a plan, a canal: Panama", true],
+      [8, "race a car", false],
+      [9, "race car", true ]]
+    test_cases.each do |case_num, input, expected|
+      unless (result = is_palindrome(input)) == expected
+        puts "Test Case: #{case_num}, Input: #{input}, Actual Result: #{result}, Expected: #{expected}"
+        raise RuntimeError
+      end
+    end
   rescue RuntimeError => e
     puts e.message + ": " + e.backtrace.inspect
   else
     puts "All test cases passed!"
-  end    
+  end
 end
