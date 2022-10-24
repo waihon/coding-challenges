06fd504a804a7daaf058572533ac1d527594a39f
Waihon Yew <yewwaihon@gmail.com>
2022-10-24 08:56:28 +0000

    Add practices for stacks

---
+++ leetcode/stacks/minimum_remove_to_make_valid_parentheses_practice.rb
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+# Given a string s of '(' , ')' and lowercase English characters.
+# Your task is to remove the minimum number of parentheses ( '(' or ')',
+# in any positions ) so that the resulting parentheses string is valid
+# and return any valid string.
+
+# @param {String} s
+# @return {String}
+def min_remove_to_make_valid(s)
+end
---
+++ leetcode/stacks/valid_parentheses_practice.rb
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/valid-parentheses/
+# Given a string s containing just the characters '(', ')', '{', '}',
+# '[' and ']', determine if the input string is valid.
+
+# @param {String} s
+# @return {Boolean}
+def is_valid(s)
+end
