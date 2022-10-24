b86a45479e0755bfc4977513195af786dfb62af4
Waihon Yew <yewwaihon@gmail.com>
2022-10-24 09:50:50 +0000

    Practice valid-parentheses

--- leetcode/stacks/valid_parentheses_practice.rb
+++ leetcode/stacks/valid_parentheses_practice.rb
@@ -5,4 +5,41 @@
 # @param {String} s
 # @return {Boolean}
 def is_valid(s)
+  parentheses_hash = { "(" => ")", "{" => "}",  "[" => "]" }
+  parentheses_stack = [] 
+
+  s.split("").each do |parenthesis|
+    if parentheses_hash[parenthesis]
+      parentheses_stack.push(parenthesis)
+    else
+      opening_parenthesis = parentheses_stack.pop
+      correct_parenthesis = parentheses_hash[opening_parenthesis]
+      if parenthesis != correct_parenthesis
+        return false
+      end
+    end
+  end
+
+  return parentheses_stack.length.zero?
+end
+
+if __FILE__ == $0
+  begin
+    test_cases = [[1, "{([])}", true],
+                  [2, "{[]()}", true],
+                  [3, "{([])]", false],
+                  [4, "{([)]}", false],
+                  [5, "{([]", false]
+                 ]
+    test_cases.each do |case_num, input, expected|
+      unless (result = is_valid(input)) == expected 
+        puts "Test Case: #{case_num}, Input: #{input}, Actual Result: #{result}, Expected: #{expected}"
+        raise RuntimeError
+      end
+    end
+  rescue RuntimeError => e
+    puts e.message + ": " + e.backtrace.inspect
+  else
+    puts "All test cases passed!"
+  end    
 end
